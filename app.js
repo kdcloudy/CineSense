b1e75c1e8d2c526ccdb802e8bbf498b8a2eb09ea
Sumit <sumit0311@gmail.com>
2020-05-16 12:55:40 +0530

    Some Flash Messages Added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-Hello
 var express = require("express");
 var mongoose = require("mongoose"),
     passport = require("passport"),
+    flash = require("connect-flash"),
     bodyParser = require("body-parser"),
     User = require("./models/user"),
     LocalStrategy = require("passport-local"),
@@ -15,6 +15,8 @@ var request= require("request");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/views'));
+
+app.use(flash());
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -35,6 +37,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
@@ -62,9 +66,11 @@ app.post("/register", function(req,res){
     User.register(new User({username:req.body.username}), req.body.password, function(err, user){
         if(err){
             console.log(err);
+            req.flash("error", "User Already Exists!");
             return res.render('register');
         }
         passport.authenticate("local")(req, res, function(){
+            req.flash("success", "You are now Registered Successfully!!");
             res.redirect("/search"); //CHANGE
         });
     });
@@ -88,6 +94,7 @@ app.post("/login",passport.authenticate("local", {
 //3) LOGOUT
 app.get("/logout", function(req, res){
     req.logout();
+    req.flash("success", "Log Out Successful!!");
     res.redirect("/");
 });
 
